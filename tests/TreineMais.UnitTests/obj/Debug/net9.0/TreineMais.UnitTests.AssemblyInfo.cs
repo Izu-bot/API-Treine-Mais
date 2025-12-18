bccdc40d33091fb0da1f7cf37fb24acfade4e25d
Kauan Cardoso <kauanmartins977@gmail.com>
2025-12-18 20:12:37 -0300

    folder

--- tests/TreineMais.UnitTests/obj/Debug/net9.0/TreineMais.UnitTests.AssemblyInfo.cs
+++ tests/TreineMais.UnitTests/obj/Debug/net9.0/TreineMais.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreineMais.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214b627689eff33a22404720df7e2b219416c8e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7539b0a1f2818f478f324b009a007763d12187db")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreineMais.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreineMais.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
