687a4deaf51d93fcf4a8ca32b74a5ee29c9bda09
Kauan Cardoso <kauanmartins977@gmail.com>
2026-01-15 19:47:58 -0300

    feat(api): Nova rota para login de usuario e alterações nos diretorios internos do projeto

--- src/TreineMais.API/Endpoints/AuthEndpoints.cs
+++ src/TreineMais.API/Endpoints/AuthEndpoints.cs
@@ -2,10 +2,12 @@ using System;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Sprache;
+using TreineMais.Application.DTO.Auth;
 using TreineMais.Application.DTO.Profile;
 using TreineMais.Application.DTO.User;
 using TreineMais.Application.UseCase.ConfirmEmail;
 using TreineMais.Application.UseCase.CreateUser;
+using TreineMais.Application.UseCase.LoginUser;
 
 namespace TreineMais.API.Endpoints;
 
@@ -17,6 +19,7 @@ internal static class AuthEndpoints
 
         group.MapPost("/register", CreateAccount);
         group.MapGet("/confirm-email", ConfirmEmail);
+        group.MapPost("/login", Login);
 
         return app;
     }
@@ -49,5 +52,27 @@ internal static class AuthEndpoints
     {
         await handler.ExecuteAsync(new ConfirmEmailCommand(token));
         return Results.Ok("E-mail confirmado.");
+    }
+
+    private static async Task<IResult> Login(
+        [FromBody] AuthRequest request,
+        [FromServices] IMediator _mediator
+    )
+    {
+        var command = new LoginUserCommand
+        {
+            Email = request.Email,
+            Password = request.Password
+        };
+
+        var result = await _mediator.Send(command);
+
+        Dictionary<string, string> tokens = new()
+        {
+            {"AccesToken", result.AccessToken},
+            {"RefreshToken", result.RefreshToken}
+        };
+
+        return Results.Ok(tokens);
     }
 }
