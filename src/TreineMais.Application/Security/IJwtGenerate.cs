fab7a460850907f38807a46d9bf47e3bda8b3532
Kauan Cardoso <kauanmartins977@gmail.com>
2026-01-15 19:46:13 -0300

    refact(application): Mudança no parâmetro solicitado para User

--- src/TreineMais.Application/Security/IJwtGenerate.cs
+++ src/TreineMais.Application/Security/IJwtGenerate.cs
@@ -1,8 +1,18 @@
 using TreineMais.Application.DTO.Auth;
+using TreineMais.Domain.Entity;
 
 namespace TreineMais.Application.Security;
 
 public interface IJwtGenerate
 {
-    string GenerateJwt(AuthRequest request);
+    /// <summary>
+    /// O parâmetro como o tipo de dado User parece fazer mais sentido aqui
+    /// não vejo o porque de colocar um DTO como AuthRequest, para tratar
+    /// ele no handler que efetua o login e gera o token parece ser mais
+    /// complicado, posso estar errado em colocar o objeto User, porém é
+    /// como ficará por enquanto 
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    string GenerateJwt(User request);
 }
